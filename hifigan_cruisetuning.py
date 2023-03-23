236c1aeb3cc25a00425af4e0e1777453f5e9602b
liam-kelley <liam.kelley.144@gmail.com>
2023-03-23 16:34:12 +0100

    conf/hifigan fixes...

--- hifigan_cruisetuning.py
+++ hifigan_cruisetuning.py
@@ -9,6 +9,7 @@ from nemo.utils.exp_manager import exp_manager
 def main(cfg):
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
+    
     model = HifiGanModel(cfg=cfg.model, trainer=trainer)
     model.maybe_init_from_pretrained_checkpoint(cfg=cfg)
     trainer.fit(model)

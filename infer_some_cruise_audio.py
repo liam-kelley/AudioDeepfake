1ad041b0a519abd65f27481c38b9184e78f387ad
liam-kelley <liam.kelley.144@gmail.com>
2023-03-28 19:04:39 +0000

    new audios testing 3

--- infer_some_cruise_audio.py
+++ infer_some_cruise_audio.py
@@ -11,7 +11,8 @@ hifigan_from_pretrained=False
 texts_to_say= [
     "I think it's... Really just as an actor",
     "Very fun... Uh... Character to play. challenging to play and I make enormously entertaining for an audience.",
-    "The movie opens almost at the end, of a movie.", 
+    "The movie opens almost at the end",
+    "of a movie.", 
     "And... this woman who helps him... uhhh and helps these other people and it starts this relationship between them",
     "just a dialogue and she actually has... his old job.",
     "at the hundred and tenth... and... he is intrigued by her voice and by... her abilities... and goes back",
